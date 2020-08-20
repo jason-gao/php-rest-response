0a6d1db4d9afbc5bb2e5d1fbd024f050942fe8aa
jason-gao <gaolu@yundun.com>
2020-08-20 18:39:19 +0800

    init

---
+++ src/Response.php
@@ -0,0 +1,178 @@
+<?php
+
+namespace Response;
+
+use Response\Exceptions\ResponseException;
+
+class Response {
+
+	//code=>message map
+	public static $codeConf;
+
+	public static $type = [ 'json', 'jsonp', 'xml' ];
+
+	public static function isCorrectJson( $body, $assoc = true ) {
+		$content    = json_decode( $body, $assoc );
+		$json_error = json_last_error();
+		if ( $json_error == JSON_ERROR_NONE ) {
+			if ( is_null( $content ) ) {
+				return '';
+			}
+
+			return $content;
+		}
+
+		return false;
+	}
+
+	public static function jsonEncodeHold( $data, $isPretty = 0 ) {
+		if ( $isPretty ) {
+			$result = json_encode( $data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT );
+		} else {
+			$result = json_encode( $data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES );
+		}
+
+		$json_error = json_last_error();
+		if ( $json_error !== JSON_ERROR_NONE ) {
+			throw new ResponseException( "json encode error: " . json_last_error_msg() );
+
+		}
+
+		return $result;
+	}
+
+	public static function jsonDecodeHold( $data, $assoc = true ) {
+		$result = self::isCorrectJson( $data, $assoc );
+
+		if ( ! $result ) {
+			throw new ResponseException( "json decode error: " . json_last_error_msg() );
+		}
+
+		return $result;
+	}
+
+
+	public static function xml_encode( $data, $encoding = 'utf-8', $root = 'Yundun' ) {
+		$xml = '<?xml version="1.0" encoding="' . $encoding . '"?>';
+		$xml .= '<' . $root . '>';
+		$xml .= self::data_to_xml( $data );
+		$xml .= '</' . $root . '>';
+
+		return $xml;
+	}
+
+
+	public static function data_to_xml( $data ) {
+		$xml = '';
+		foreach ( $data as $key => $val ) {
+			is_numeric( $key ) && $key = "item id=\"$key\"";
+			$xml .= "<$key>";
+			$xml .= ( is_array( $val ) || is_object( $val ) ) ? self::data_to_xml( $val ) : $val;
+			list( $key, ) = explode( ' ', $key );
+			$xml .= "</$key>";
+		}
+
+		return $xml;
+	}
+
+
+	public static function XML2Array( $xml, $recursive = false ) {
+		if ( ! $recursive ) {
+			$array = simplexml_load_string( $xml );
+		} else {
+			$array = $xml;
+		}
+		$newArray = array();
+		$array    = ( array ) $array;
+		foreach ( $array as $key => $value ) {
+			$value = ( array ) $value;
+			if ( isset ( $value [0] ) ) {
+				$newArray [ $key ] = trim( $value [0] );
+			} else {
+				$newArray [ $key ] = self::XML2Array( $value, true );
+			}
+		}
+
+		return $newArray;
+	}
+
+	public static function redirect307( $url, $replace = true, $exit = 1 ) {
+		header( "Location: $url", $replace, 307 );
+		$exit && exit();
+	}
+
+	public static function responseHttpCode( $httpCode = 200 ) {
+		http_response_code( $httpCode );
+	}
+
+
+	public static function response( $data, $type = 'json', $isPrettyJson = 0, $exit = 1 ) {
+		$type = strtolower( $type );
+
+		if ( ! in_array( $type, self::$type ) ) {
+			$jsonStr = implode( "/", self::$type );
+			throw new ResponseException( "response type $type not support , just support $jsonStr" );
+		}
+
+		switch ( $type ) {
+			case 'json':
+				if ( ! headers_sent() ) {
+					header( 'Content-Type:application/json; charset=utf-8' );
+				}
+				echo( self::jsonEncodeHold( $data, $isPrettyJson ) );
+				$exit && exit();
+				break;
+			case 'jsonp':
+				if ( ! headers_sent() ) {
+					header( 'Content-Type:application/json; charset=utf-8' );
+				}
+				$handler = strtolower( 'callback' );
+				echo( $handler . '(' . self::jsonEncodeHold( $data, $isPrettyJson ) . ');' );
+				$exit && exit();
+				break;
+			case 'xml':
+				if ( ! headers_sent() ) {
+					header( 'Content-Type:text/xml; charset=utf-8' );
+				}
+				echo( self::xml_encode( $data ) );
+				$exit && exit();
+				break;
+		}
+
+		return true;
+	}
+
+
+	public static function setCodeConf( $conf ) {
+		self::$codeConf = $conf;
+	}
+
+
+	public static function responseApi( $code, $data, $codeParams = [], $message = '', $type = 'json', $isPrettyJson = 0, $exit = 1 ) {
+		if ( is_null( self::$codeConf ) ) {
+			throw new ResponseException( "please set code conf" );
+		}
+
+		if ( ! isset( self::$codeConf[ $code ] ) ) {
+			throw new ResponseException( "code $code not exists, please conf it" );
+		}
+		$response = [
+			'status' => [
+				'code'      => $code,
+				'message'   => $message,
+				'create_at' => date( 'Y-m-d H:i:s' )
+			],
+			'data'   => $data
+		];
+		if ( ! isset( $response['status']['message'] ) || empty( $response['status']['message'] ) ) {
+			$response['status']['message'] = vsprintf( $codeParams ? self::$codeConf[ $code ] : ( str_replace( [
+				'%s',
+				'%d'
+			], '', self::$codeConf[ $code ] ) ), ( $codeParams ? array_values( $codeParams ) : [] ) );
+		}
+
+		self::response( $response, $type, $isPrettyJson, $exit );
+	}
+
+
+}
---
+++ tests/ResponseTest.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace Tests;
+
+
+use Response\Config\Config;
+use Response\Response;
+
+class ResponseTest extends TestCase {
+
+
+	public function testResponse() {
+		$conf = Config::load( __DIR__ . '/../code.conf' );
+//		var_dump( $conf['code'] );
+
+		Response::setCodeConf( $conf['code'] );
+		Response::responseApi( 1, [ 'test' => 1 ], [] );
+//		Response::responseApi( 1, [ 'test' => 1 ], [] ,'', 'jsonp');
+//		Response::responseApi( 1, [ 'test' => 1 ], [] ,'', 'xml');
+//		Response::responseApi(6000000, ['test' => 1], ['1', '2']);
+//		Response::responseApi(6000000, ['test' => 1], ['1', '2'], '111');
+//		Response::responseApi(1, ['test' => 1], [], '', 'xml');
+	}
+
+
+}
